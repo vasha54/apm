035aef5235f78d3dcd78734f4c825c09b8abdfae
vasha54 <luis.valido1989@gmail.com>
2023-04-10 21:17:52 -0400

    Optimizando grafica de histograma utilizando matplotlib

--- view/components/charts/chart_histogram.py
+++ view/components/charts/chart_histogram.py
@@ -11,18 +11,11 @@ class ChartHistogram(FigureCanvas):
     def __init__(self,parent):
         self.fig,self.ax = plt.subplots(1,1,constrained_layout=True)
         super().__init__(self.fig)
+        self.data = {}
         self.setParent(parent)
         
     def makeHistogramChart(self,_data):
-        
-        colorText = PreferenceGUI.instance().getValueSettings(PreferenceGUI.COLOR_TEXT_CHART)
-        colorBackground = PreferenceGUI.instance().getValueSettings(PreferenceGUI.COLOR_BACKGROUND_CHART)
-        colorAxes = PreferenceGUI.instance().getValueSettings(PreferenceGUI.COLOR_AXES_CHART)
-        
-        
-        placeDecimal = int(PreferenceGUI.instance().getValueSettings(PreferenceGUI.DECIMAL_PLACES))
-        formatStr = '.'+str(placeDecimal)+'f'
-        
+        self.data=_data
         
         x = list()
         y1 = _data['coef']
@@ -31,27 +24,48 @@ class ChartHistogram(FigureCanvas):
             x.append(i)
         
         plt.bar(x, height=y1)
-       
-        
-        self.ax.set_title(_data['name'],fontsize=10,color=colorText)
+        self.ax.set_title(_data['name'],fontsize=10,color='#000000')
         xlabel = ''
         ylabel = ''
         if 'title_X' in _data.keys():
             xlabel=_data['title_X']
-            self.ax.set_xlabel(xlabel,fontsize=8,color=colorText)
+            self.ax.set_xlabel(xlabel,fontsize=8,color='#000000')
         if 'title_Y' in _data.keys():
             ylabel=_data['title_Y']
-            self.ax.set_ylabel(ylabel,fontsize=8,color=colorText)
-        self.fig.patch.set_facecolor('#000000FF')
-        self.fig.patch.set_alpha(0)
-        self.ax.set_facecolor(colorBackground)
-        self.ax.tick_params(axis='x', colors=colorAxes)    
-        self.ax.tick_params(axis='y', colors=colorAxes)
-        self.ax.spines['left'].set_color(colorAxes)        
-        self.ax.spines['bottom'].set_color(colorAxes)
+            self.ax.set_ylabel(ylabel,fontsize=8,color='#000000')
+        
+    
+    def setColorText(self,_color):        
+        self.ax.set_title(self.data['name'],fontsize=10,color=_color)
+        xlabel = ''
+        ylabel = ''
+        if 'title_X' in self.data.keys():
+            xlabel=self.data['title_X']
+            self.ax.set_xlabel(xlabel,fontsize=8,color=_color)
+        if 'title_Y' in self.data.keys():
+            ylabel=self.data['title_Y']
+            self.ax.set_ylabel(ylabel,fontsize=8,color=_color)
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+        
+    def setColorAxes(self,_color):
+        self.ax.tick_params(axis='x', colors=_color)    
+        self.ax.tick_params(axis='y', colors=_color)
+        self.ax.spines['left'].set_color(_color)        
+        self.ax.spines['bottom'].set_color(_color)
         self.ax.spines['top'].set_alpha(0)        
         self.ax.spines['right'].set_alpha(0)
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
         
+    def setColorBackground(self,_color):
+        self.fig.patch.set_facecolor('#000000FF')
+        self.fig.patch.set_alpha(0)
+        self.ax.set_facecolor(_color)
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+        
+    
         
         
         
